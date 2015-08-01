c1e95d2cbf32eacd7cb02a8cfb9e325d24127dc4
Prateek Jadhwani <prateekjadhwani@users.noreply.github.com>
2015-08-01 11:59:37 -0400

    A working version of content management system

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # podcast-player-content-manager
+
 A content manager system for The Web Platform Today site.
+
+## Steps to add an episode content
+
+- In the `episodes` folder create a file called `episode_num.json`. For example, if the episode number is **45**, then you will need to create the file called `045.json`.
+- Start adding content in json format. Please take a look at the acceptable content section for more details.
+- From the terminal, run the command `node index.js`.
+- Once the process is complete, you will see the file `content.json` in the root directory.
+
+## Acceptable Content
+
+A content file should have the following keys
+
+| key | type | Description |
+|----|-----|----|
+| title | String | The title of the episode |
+| episode | String | The episode number |
+| coverart |  String | The cover art of the episode |
+| summary | String | The summary of the episode |
+| mp3 | String | The location of the mp3 file |
+| resources | Array of Objects | The resources used in this episode. Consists of a `title` and a `link` |
+
+**Note**: The local mp3 file needs to be a local copy of the podcast file in the `mp3` folder. This local file will never be promoted. It is in the mp3 folder just to generate the waveform. Once the `datapoints` are generated, feel free to delete the local copy of the mp3.
+
+Example
+
+```json
+{
+  "title": "Diving into Angular 2",
+  "episode": "53",
+  "coverart": "http://assets.libsyn.com/content/9454132",
+  "summary": "Pascal Precht (@PascalPrecht), Senior Software Engineer at Thoughtram & creator of ng-translate, chats with us about the Angular 2 and how developers can get ready today.",
+  "localmp3": "../mp3/episode-53_diving-into-angular-2.mp3",
+  "mp3": "https://s3.amazonaws.com/thewebplatform/episode-53_diving-into-angular-2.mp3",
+  "resources": [
+    {
+      "title": "Angular 2",
+      "link": "http://angular.io"
+    },
+    {
+      "title": "TypeScript",
+      "link": "http://www.typescriptlang.org/"
+    },
+    {
+      "title": "Definitely Typed",
+      "link": "https://github.com/DefinitelyTyped"
+    }
+  ]
+}
+```
---
+++ index.js
@@ -0,0 +1,163 @@
+var fs = require("fs"),
+		clc = require('cli-color'),
+		jsonfile = require('jsonfile'),
+		util = require('util'),
+		waveform = require('waveform');
+
+var EPISODE_DIR = 'episodes';
+
+var colors = {
+	error: clc.red,
+	notice: clc.blue,
+	warn: clc.yellow,
+	info: clc.cyan,
+	success: clc.green
+};
+
+/*
+ * Reads a directory for files
+ */
+function readDirectory(dir, readFileCallback) {
+	process.stdout.write(clc.erase.screen);
+	console.log( colors.info( "Reading Directory '" +
+													 colors.notice(dir) +
+													 "' for episodes") );
+	spacer();
+
+	// start reading the directory
+	fs.readdir(dir, function(err, files) {
+		console.log( colors.notice( "\nFile Names Obtained : ") );
+		console.log( colors.success(files) );
+		spacer();
+		readFileCallback(dir, files);
+	});
+}
+
+function readAndProcessFiles(dir, files) {
+	console.log( colors.info("\n\nProcessing Episodes") );
+	spacer();
+
+	var numOfFilesReady = 0;
+	files.forEach( function (filename, index) {
+		var fileData = jsonfile.readFileSync(dir + '/' + filename);
+		if(validateJSONFile(dir, fileData, filename)) {
+			numOfFilesReady++;
+		}
+	});
+	if(numOfFilesReady == files.length) {
+		contentInit();
+	}
+}
+
+function validateJSONFile(dir, fileData, filename) {
+	var isValid = true;
+	console.log( colors.notice('Reading file for episode ' +
+								 colors.success(fileData.episode) +
+								 ' with episode title : ' +
+								 colors.success(fileData.title)) );
+		
+	if( !fileData.localmp3 ) {
+		console.log( colors.error("There is no mp3 associated with " +
+															fileData.episode +
+															" episode"));
+	}
+	
+	if(fileData.datapoints) {
+		console.log( colors.notice("This episode has valid data") );
+	}
+
+	if( fileData.localmp3 && !fileData.datapoints ) {
+		console.log( colors.warn( "The episode does not have a waveform generated " +
+														 "for its mp3 file.\nStarting waveform generation " +
+														 "process in background.\nPlease be patient...") );
+		generateWaveform(dir, fileData, filename);
+		isValid = false;
+	}
+	spacer();
+	return isValid;
+}
+
+/*
+ *	Generates Waveform for a given file
+ */
+function generateWaveform(dir, fileData, filename) {
+	waveform( dir + '/' + fileData.localmp3, {
+		// waveform.js options
+		waveformjs: '-',  // path to output-file or - for stdout
+		'wjs-width': 800,             // width in samples
+		'wjs-precision': 4,           // how many digits of precision
+		'wjs-plain': false
+		}, function(err, output) {
+		if( err ) {
+			console.log( colors.error(err));
+		}
+		if( output ) {
+			console.log( colors.success("Waveform generated for " +
+																 filename));
+			appendWaveformToEpisode(dir + '/' + filename, output, fileData);
+		}
+	});
+}
+
+/*
+ *	Appends Waveform data to the episode file
+ */
+
+function appendWaveformToEpisode(filename, output, fileData) {
+	console.log( colors.notice("Adding waveform to episode file " +
+																 filename));
+	fileData['datapoints'] = JSON.parse(output);
+	jsonfile.writeFileSync(filename, fileData, {
+		spaces: 2
+	});
+	console.log( colors.success(filename + ' now has all the data points for mp3.'));
+	spacer();
+	contentInit();
+}
+
+/*
+ * Initializes Content file creation process
+ */
+function contentInit() {
+	fs.readdir(EPISODE_DIR, function(err, files) {
+		var filesWithDataPoints = 0;
+		files.forEach( function (filename, index) {
+			var fileData = jsonfile.readFileSync(EPISODE_DIR + '/' + filename);
+			if(fileData.datapoints) {
+				filesWithDataPoints ++;
+			}
+		});
+		if(filesWithDataPoints == files.length) {
+			console.log( colors.info("\n\nAll checks complete.\nStarting content creation process") );
+			spacer();
+			createContentFile();
+		}
+	});
+}
+
+function createContentFile() {
+	fs.readdir(EPISODE_DIR, function(err, files) {
+		var finalContent = [];
+		console.log( colors.notice("Merging File Data") );
+		files.forEach( function (filename, index) {
+			var fileData = jsonfile.readFileSync(EPISODE_DIR + '/' + filename);
+			finalContent.push(fileData);
+		});
+		console.log( colors.notice("Creating content file") );
+		jsonfile.writeFileSync('content.json', finalContent, {
+			spaces: 2
+		});
+		console.log( colors.success("Content file is complete\nPlease see " +
+															 colors.notice("content.json") +
+															 " file.") );
+	});
+}
+
+/*
+ * Adds a spacer line
+ */
+function spacer() {
+	console.log(colors.warn("=================================================="));
+}
+
+readDirectory( EPISODE_DIR , readAndProcessFiles);
